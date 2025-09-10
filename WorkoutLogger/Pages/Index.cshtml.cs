f790b1497452740e7461b4f3ac57d305759701ee
Martin Terziev <m.terziev08@abv.bg>
2025-09-10 18:04:08 +0300

    Added home page view with all entries displayed in a table.

--- WorkoutLogger/Pages/Index.cshtml.cs
+++ WorkoutLogger/Pages/Index.cshtml.cs
@@ -1,19 +1,26 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WorkoutLogger.Database;
 using WorkoutLogger.Database.Entities;
 
 namespace WorkoutLogger.Pages;
 
 public class IndexModel : PageModel
 {
-    public IndexModel()
+    private WorkoutLoggerDbContext _context;
+    public IndexModel(WorkoutLoggerDbContext context)
     {
-
+        _context = context;
     }
 
     public List<Workout> Workouts { get; set; }
 
     public void OnGet()
     {
+        Workouts = GetAllWorkouts();
+    }
 
+    private List<Workout> GetAllWorkouts()
+    {
+        return _context.Workouts.ToList();
     }
 }
